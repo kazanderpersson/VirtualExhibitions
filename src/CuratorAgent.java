79fad14ddc374c00cfc1b990d35eee6bbf605327
Karl-Axel Zander <kazander@kth.se>
2013-11-11 14:48:03 +0100

    ping code for curatoragent

--- src/CuratorAgent.java
+++ src/CuratorAgent.java
@@ -1,10 +1,34 @@
 import jade.core.Agent;
-
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class CuratorAgent extends Agent {
 	@Override
 	protected void setup() {
-		// TODO Auto-generated method stub
-		super.setup();
+
+		addBehaviour(new listenBehaviour());
 	}
+	
+	class listenBehaviour extends CyclicBehaviour {
+		public void action() {
+			
+			ACLMessage msg = myAgent.receive();
+			if (msg != null) {
+				if (msg.getContent().equals("ping")) {
+						ACLMessage msgBack = new ACLMessage(ACLMessage.INFORM);
+						msgBack.addReceiver(msg.getSender());
+						msgBack.setLanguage("English");
+						msgBack.setOntology("ping-message");
+						msgBack.setContent("ping");
+						send(msgBack);
+				}
+			}
+			else {
+			block();
+			}
+			
+
+			
+		}
+}
 }
