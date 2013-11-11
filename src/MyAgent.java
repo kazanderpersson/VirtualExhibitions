45b65c650411b944c7baf627a8f8d692d381f33b
anhallbe <anhallbe@gmail.com>
2013-11-11 13:35:17 +0100

    Added a System.out.print...

--- src/MyAgent.java
+++ src/MyAgent.java
@@ -3,6 +3,6 @@ import jade.core.Agent;
 public class MyAgent extends Agent {
 	protected void setup() {
 		System.out.println("Agent \"" + getAID().getName() + "\" is ready!");
+		System.out.println("Hello, this line was edited by Andreas.......");
 	}
-
 }
