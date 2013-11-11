12f881563c8294180ab34a6046481e85b9f0cbcd
Karl-Axel Zander <kazander@kth.se>
2013-11-11 13:32:56 +0100

    merge-experiment

--- src/MyAgent.java
+++ src/MyAgent.java
@@ -3,6 +3,8 @@ import jade.core.Agent;
 public class MyAgent extends Agent {
 	protected void setup() {
 		System.out.println("Agent \"" + getAID().getName() + "\" is ready!");
+		
+		//test
 	}
 
 }
